d7f26cc569d89f1ab2c94eabd1551098b8b6a4a0
Yevgeniy Davydov <yevgeniydavydov@Yevgeniys-MBP.fritz.box>
2024-01-20 15:48:28 +0100

    new repository with files

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+"use strict"
+
+// // 1 Перетворення типів
+
+// let data = 23;
+
+// let dataString = String(data);
+// console.log(dataString, typeof dataString);
+
+// let dataNumber = Number(data);
+// console.log(dataNumber, typeof dataNumber);
+
+// let dataBoolean = Boolean(data);
+// console.log(dataBoolean, typeof dataBoolean);
+
+
+// // 2 Конвертер валют 
+
+// const rate = 37.54;
+
+// let uah = +prompt("Введіть суму гривень(цiли числа або нецiли числа через крапку)");
+
+
+// let usd = uah / rate;
+
+
+// console.log(usd);
+
+
+// // 3 Обчислення вартості бензину
+
+// let liters = +prompt("Введіть кількість літрів (цiли числа або нецiли числа через крапку)");
+// let pricePerLiter = +prompt("Введіть ціну за літр (цiли числа або нецiли числа через крапку)");
+
+
+// let result = (liters * pricePerLiter).toFixed(2);
+
+
+// console.log(result);
+
+
+// // 4 Додаткова задача
+
+// let a = 10 // може бути будь-яке інше ціле додатнє число
+// let b = 20 // може бути будь-яке інше ціле додатнє число
+
+// let binaryResult = (a + b).toString(2);
+// console.log(binaryResult); // виведе в консоль бінарне число строкою
